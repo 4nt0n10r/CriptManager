d935cc048fbeaa22f82f962207adb5c05b72fd50
Antonio <antonioruz84@gmail.com>
2024-03-17 13:09:58 +0100

    Rename project

--- CriptManager/asgi.py
+++ CriptManager/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for CriptManager project.
+ASGI config for criptmanager project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CriptManager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'criptmanager.settings')
 
 application = get_asgi_application()
--- CriptManager/wsgi.py
+++ CriptManager/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for CriptManager project.
+WSGI config for criptmanager project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CriptManager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'criptmanager.settings')
 
 application = get_wsgi_application()
